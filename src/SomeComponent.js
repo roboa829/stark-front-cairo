e7fa7140378ca841f7986aab0e7f361be3daf567
roboa829 <83786756+roboa829@users.noreply.github.com>
2023-12-27 19:22:08 +0200

    Update SomeComponent.js

--- src/SomeComponent.js
+++ src/SomeComponent.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
+import React, { memo, useCallback } from 'react';
 
 const StyledDiv = styled.div`
   color: blue;
 `;
 
-const SomeComponent = () => (
-  <StyledDiv>
-    {/* Component content */}
-  </StyledDiv>
-);
+
+const SomeComponent = memo(({ onClick }) => {
+  const handleClick = useCallback(() => {
+    onClick();
+  }, [onClick]);
+
+  return (
+    <button onClick={handleClick}>Click me</button>
+  );
+});
