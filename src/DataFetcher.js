291e507b975427ee33a8c2c4bf46d0e73bd7cc77
roboa829 <83786756+roboa829@users.noreply.github.com>
2023-12-27 19:19:26 +0200

    Create DataFetcher.js

---
+++ src/DataFetcher.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+const DataFetcher = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.example.com/data')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>{data ? /* render data */ : 'Loading...'}</div>
+  );
+};
+
+export default DataFetcher;
